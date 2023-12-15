878028c7f7662ae1847b506bc7f4432f37e0f66f
TohDeKai <tohdekai@outlook.com>
2023-12-16 03:03:16 +0800

    day1 part2 solution

--- day1/day1.py
+++ day1/day1.py
@@ -29,8 +29,50 @@ def trebuchet_part1(puzzle):
     return res
 
 
+def trebuchet_part2(puzzle):
+    """
+    replace the digits with the numeric version then do part 1 again
+    """
+    # one two three four five six seven eight nine ten
+    spelt_out_digits = {
+        "one": "o1e",
+        "two": "t2e",
+        "three": "th3ee",
+        "four": "f4ur",
+        "five": "f5ve",
+        "six": "s6x",
+        "seven": "se7en",
+        "eight": "ei8ht",
+        "nine": "n9ne",
+    }
+
+    res = 0
+    for line in puzzle:
+        for key, value in spelt_out_digits.items():
+            line = line.replace(key, value)
+
+        start, end = 0, len(line) - 1
+        n = end
+        while start < n:
+            if line[start].isnumeric():
+                first = line[start]
+                break
+            else:
+                start += 1
+        while end >= 0:
+            if line[end].isnumeric():
+                last = line[end]
+                break
+            else:
+                end -= 1
+        combined = first + last
+        res += int(combined)
+    return res
+
+
 # reading input
 with open("input.txt") as f:
     puzzle = f.readlines()
 
 print("Part 1 Solution: " + str(trebuchet_part1(puzzle)))
+print("Part 2 Solution: " + str(trebuchet_part2(puzzle)))
