e1d057b894dea4cc85e1e29fe582ce84526e7ee1
TohDeKai <tohdekai@outlook.com>
2024-05-28 23:34:22 +0200

    refactor day2pt1 with hashmap

--- day2/day2.py
+++ day2/day2.py
@@ -1,7 +1,7 @@
 def part1(puzzle):
     # 12 red cubes, 13 green cubes, and 14 blue cubes
-    red, green, blue = 12, 13, 14
     res = 0
+    cubesCount = {}
     for line in puzzle:
         line = line.split(":")
         game_num = int(line[0].replace("Game ", ""))
@@ -9,25 +9,22 @@ def part1(puzzle):
         exceed = False
         for game in games:
             cubes = game.split(",")
-            red = green = blue = 0
+            cubesCount["red"] = cubesCount["blue"] = cubesCount["green"] = 0
             if exceed:
                 break
             for i in range(len(cubes)):
                 cubes[i] = cubes[i].strip().split(" ")
-                num = cubes[i][0]
-                color = cubes[i][1]
-                if color == "red":
-                    red += int(num)
-                elif color == "green":
-                    green += int(num)
-                else:
-                    blue += int(num)
-                if red > 12 or green > 13 or blue > 14:
+                [num, color] = cubes[i]
+                cubesCount[color] += int(num)
+                if (
+                    cubesCount["red"] > 12
+                    or cubesCount["green"] > 13
+                    or cubesCount["blue"] > 14
+                ):
                     exceed = True
                     break
         if not exceed:
             res += game_num
-
     return res
 
 
