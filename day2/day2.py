631035fa5d002b925744cca72897ed80eda230e9
TohDeKai <tohdekai@outlook.com>
2024-05-28 23:30:23 +0200

    solve day2pt1

--- day2/day2.py
+++ day2/day2.py
@@ -1,28 +1,32 @@
 def part1(puzzle):
     # 12 red cubes, 13 green cubes, and 14 blue cubes
-
-    """
-    ur curr sol is wrong because it doesn't account for if no red or no green
-    """
     red, green, blue = 12, 13, 14
     res = 0
     for line in puzzle:
         line = line.split(":")
-        game_num = line[0].replace("Game ", "")
+        game_num = int(line[0].replace("Game ", ""))
         games = line[1].split(";")
+        exceed = False
         for game in games:
             cubes = game.split(",")
-            for i in range(3):
-                cubes[i] = cubes[i].replace("red", "")
-                cubes[i] = cubes[i].replace("green", "")
-                cubes[i] = cubes[i].replace("blue", "")
-                cubes[i] = cubes[i].strip()
-                cubes[i] = int(cubes[i])
-                print(cubes[i])
-            if cubes[0] > red or cubes[1] > green or cubes[2] > blue:
-                continue
-            else:
-                res += game_num
+            red = 0
+            green = 0
+            blue = 0
+            for i in range(len(cubes)):
+                cubes[i] = cubes[i].strip().split(" ")
+                num = cubes[i][0]
+                color = cubes[i][1]
+                if color == "red":
+                    red += int(num)
+                elif color == "green":
+                    green += int(num)
+                else:
+                    blue += int(num)
+                if red > 12 or green > 13 or blue > 14:
+                    exceed = True
+        if not exceed:
+            res += game_num
+
     return res
 
 
